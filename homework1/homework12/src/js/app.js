da7484d11236dfb95cc1eb0361df1ec01d5d926c
NickolayLabazov <46165845+NickolayLabazov@users.noreply.github.com>
2019-01-29 19:09:56 +0300

    Update app.js

--- homework1/homework12/src/js/app.js
+++ homework1/homework12/src/js/app.js
@@ -1,7 +1,8 @@
 "use strict";
   
 const balanseWrite=(string, numberOfBalanse)=>{                          // Объявление функции вывода баланса
-    let ost10=numberOfBalanse%10;                                        // Определение остатка от деления на 10 для выбора окончания слова ""Балл
+    let str = string[0];
+	let ost10=numberOfBalanse%10;                                        // Определение остатка от деления на 10 для выбора окончания слова ""Балл
     let ost100=numberOfBalanse%100;                                      // Определение остатка от деления на 100 для выбора окончания слова ""Балл
     let ball = " баллов";
     if ((ost10==1) & ((ost100<11)||(ost100>19))){
@@ -10,18 +11,22 @@ const balanseWrite=(string, numberOfBalanse)=>{                          // Об
     else if (((ost10>=2)&(ost10<=4))&((ost100<11)||(ost100>19))){
       ball = " балла";
     }
-    document.write(`${string}${numberOfBalanse}${ball}<br>`);            // Вывод на экран сообщения о балансе
+    return `${str}${numberOfBalanse}${ball}`;                            // Возвращение сообщения о балансе
 }
 
-const string = 'Ваш баланс: ';
 let numberOfBalanse = 523;
-balanseWrite(string, numberOfBalanse);                                   // Вызов функции вывода баланса
-
+let string = balanseWrite`Ваш баланс: ${numberOfBalanse}`;               // Вызов функции вывода баланса
+console.log(string);                                                     // Вывод баланса в консоль 
+ 
 numberOfBalanse = 6000;
-balanseWrite(string, numberOfBalanse);
+string = balanseWrite`Ваш баланс: ${numberOfBalanse}`;
+console.log(string);
+
 
 numberOfBalanse = 5001;
-balanseWrite(string, numberOfBalanse);
+string = balanseWrite`Ваш баланс: ${numberOfBalanse}`;
+console.log(string);
 
 numberOfBalanse = 5013;
-balanseWrite(string, numberOfBalanse);
+string = balanseWrite`Ваш баланс: ${numberOfBalanse}`;
+console.log(string);
